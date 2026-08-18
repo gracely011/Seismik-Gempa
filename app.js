10b9a257d9b32d6d17acf464c8a218fcbe084757
gracely011 <gracely011@users.noreply.github.com>
2026-08-18 19:57:55 +0700

    fix(ui): fix bookmark location icon rendering and bump pwa cache to v14.6

--- app.js
+++ app.js
@@ -1427,7 +1427,7 @@ function toggleSaveCurrentArea() {
         places.splice(existingIndex, 1);
         savePlacesList(places);
         if (btnEl) btnEl.classList.remove("active");
-        if (btnIcon) btnIcon.innerHTML = '<path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z"/>';
+        if (btnIcon) btnIcon.innerHTML = '&#xe867;';
     } else {
         const newPlace = {
             id: 'place_' + Date.now(),
@@ -1441,7 +1441,7 @@ function toggleSaveCurrentArea() {
         places.unshift(newPlace);
         savePlacesList(places);
         if (btnEl) btnEl.classList.add("active");
-        if (btnIcon) btnIcon.innerHTML = '<path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/>';
+        if (btnIcon) btnIcon.innerHTML = '&#xe866;';
     }
 
     renderSavedPlacesUI();
@@ -1457,9 +1457,7 @@ function updateBookmarkIconState() {
     const btnEl = document.getElementById("btnBookmarkArea");
     if (btnEl) btnEl.classList.toggle("active", isSaved);
     if (btnIcon) {
-        btnIcon.innerHTML = isSaved
-            ? '<path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/>'
-            : '<path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z"/>';
+        btnIcon.innerHTML = isSaved ? '&#xe866;' : '&#xe867;';
     }
 }
 
